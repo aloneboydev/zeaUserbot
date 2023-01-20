b08088adea1c1534c607bf1a225442e0b5584f7b
amangtele <122607345+amangtele@users.noreply.github.com>
2023-01-20 20:08:28 +0700

    BISMILLAH

---
+++ assistant/manager/__init__.py
@@ -0,0 +1,11 @@
+# Amang Userbot
+# Copyright (C) 2021-2022 amangtele
+#
+# This file is a part of < https://github.com/amangtele/AmangUserbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/amangtele/AmangUserbot/blob/main/LICENSE/>.
+
+from Amang._misc._decorators import amang_cmd
+from Amang.fns.helper import inline_mention
+
+from .. import *
---
+++ assistant/manager/_on_adds.py
@@ -0,0 +1,23 @@
+# Amang Userbot
+# Copyright (C) 2021-2022 amangtele
+#
+# This file is a part of < https://github.com/amangtele/AmangUserbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/amangtele/AmangUserbot/blob/main/LICENSE/>.
+
+from telethon import events
+
+from . import *
+
+
+@asst.on(events.ChatAction(func=lambda x: x.user_added))
+async def dueha(e):
+    user = await e.get_user()
+    if not user.is_self:
+        return
+    sm = udB.get_key("ON_MNGR_ADD")
+    if sm == "OFF":
+        return
+    if not sm:
+        sm = "Terima kasih telah Menambahkan saya :)"
+    await e.reply(sm, link_preview=False)
