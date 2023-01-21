641b0138ee9bb9afdc404c5b7f8cf09821909bac
amangtele <122607345+amangtele@users.noreply.github.com>
2023-01-22 03:05:09 +0700

    Update Kalkulator.py

--- plugins/Kalkulator.py
+++ plugins/Kalkulator.py
@@ -149,6 +149,6 @@ async def _(e):
         "÷",
     ]
     tamangd = [Button.inline(f"{x}", data=f"calc{x}") for x in m]
-    lst = list(zip(tamangd[::4], amangad[1::4],amangrad[2::4]amangyrad[3::4]))
+    lst = list(zip(tamangd[::4], tamangad[1::4], tamangd[2::4], tamangd[3::4]))
     lst.append([Button.inline("=", data="calc=")])
     await e.edit(get_string("calc_1"), buttons=lst)
