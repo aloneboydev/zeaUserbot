61aefb2e0b818b5a2f2a7f71e51ef7a8f2fbe3ff
amangtele <122607345+amangtele@users.noreply.github.com>
2023-01-22 03:31:19 +0700

    Update autocorrect.py

--- addons/autocorrect.py
+++ addons/autocorrect.py
@@ -1,9 +1,9 @@
-# Ultroid - UserBot
-# Copyright (C) 2021-2022 TeamUltroid
+# Amang Userbot
+# Copyright (C) 2021-2022 amangtele
 #
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# This file is a part of < https://github.com/amangtele/AmangUserbot/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# <https://www.github.com/amangtele/AmangUserbot/blob/main/LICENSE/>.
 
 from . import get_help
 
