6a9f4f1485f03bde584deb2c58463ea0c5cba8b5
amangtele <122607345+amangtele@users.noreply.github.com>
2023-01-22 13:07:21 +0700

    Update asupan.py

--- plugins/asupan.py
+++ plugins/asupan.py
@@ -17,94 +17,26 @@
 """
 
 
-from secrets import choice
+import random
+from telethon.tl.types import InputMessagesFilterVideo
 
-from telethon.tl.types import InputMessagesFilterVideo, InputMessagesFilterVoice
-import asyncio
-from asyncio import gather
-from random import choice
-from pyrogram import Client, filters, enums
-from pyrogram.types import ChatPermissions, ChatPrivileges, Message
-from pyrogram import Client as amang
-import string
-from time import sleep
-from . import (
-    eor,
-    amang_cmd,
-)
+from . import amang_cmd
 
-
-@amang_cmd(pattern="asupan$")
-async def _(event):
-    xx = await edit_or_reply(event, "`Tunggu Sebentar...`")
+@amang_cmd(pattern="asupan( (.*)|$)")
+async def asupan(event):
     try:
         asupannya = [
             asupan
             async for asupan in event.client.iter_messages(
-                "@tedeasupancache", filter=InputMessagesFilterVideo
+                "@asupancilikbot", filter=InputMessagesFilterVideo
             )
         ]
+        aing = await event.client.get_me()
         await event.client.send_file(
-            event.chat_id, file=choice(asupannya), reply_to=event.reply_to_msg_id
+            event.chat_id,
+            file=random.choice(asupannya),
+            caption=f"Nih kak asupannya [{owner}](tg://user?id={aing.id})",
         )
-        await xx.delete()
+        await event.delete()
     except Exception:
-        await xx.edit("**Tidak bisa menemukan video asupan.**")
-
-
-@amang_cmd(pattern="desahcewe$")
-async def _(event):
-    if event.chat_id in BLACKLIST_CHAT:
-        return await edit_or_reply(
-            event, "**Perintah ini Dilarang digunakan di Group ini**"
-        )
-    xx = await edit_or_reply(event, "`Tunggu Sebentar...`")
-    try:
-        desahcewe = [
-            desah
-            async for desah in event.client.iter_messages(
-                "@desahancewesangesange", filter=InputMessagesFilterVoice
-            )
-        ]
-        await event.client.send_file(
-            event.chat_id, file=choice(desahcewe), reply_to=event.reply_to_msg_id
-        )
-        await xx.delete()
-    except Exception:
-        await xx.edit("**Tidak bisa menemukan desahan cewe.**")
-
-
-@amang_cmd(pattern="desahcowo$")
-async def _(event):
-    if event.chat_id in BLACKLIST_CHAT:
-        return await edit_or_reply(
-            event, "**Perintah ini Dilarang digunakan di Group ini**"
-        )
-    xx = await edit_or_reply(event, "`Tunggu Sebentar...`")
-    try:
-        desahcowo = [
-            desah
-            async for desah in event.client.iter_messages(
-                "@desahancowokkkk", filter=InputMessagesFilterVoice
-            )
-        ]
-        await event.client.send_file(
-            event.chat_id, file=choice(desahcowo), reply_to=event.reply_to_msg_id
-        )
-        await xx.delete()
-    except Exception:
-        await xx.edit("**Tidak bisa menemukan desahan cowo.**")
-
-
-CMD_HELP.update(
-    {
-        "asupan": f"**Plugin : **`asupan`\
-        \n\n  »  **Perintah :** `{cmd}asupan`\
-        \n  »  **Kegunaan : **Untuk Mengirim video asupan secara random.\
-        \n\n  »  **Perintah :** `{cmd}desahcowo`\
-        \n  »  **Kegunaan : **Untuk Mengirim voice desah cowo secara random.\
-        \n\n  »  **Perintah :** `{cmd}desahcewe`\
-        \n  »  **Kegunaan : **Untuk Mengirim voice desah cewe secara random.\
-    "
-    }
-)
+        await event.edit("Tidak bisa menemukan video asupan.")
